c852a27ead6a834f3eb6446b54620fd1bb2e16c6
Alex Albino <webmaster@alexventure.com>
2015-04-09 20:48:05 -0700

    Removing non SAM related framework components, adding in OpenOCD config (for non Windows boot loading support)

---
+++ blinky/blinky/asf-3.21.0/thirdparty/ijg/docsrc/documentation.h
@@ -0,0 +1,11 @@
+/**
+ * \defgroup group_avr32_services_graphics_codec_jpg_ijg IJG JPEG Decoder
+ *
+ * The IJG (the Independent JPEG Group) JPEG decoder offers, besides JPEG decoding,
+ * basic scaling (factors n*2) and an option to implement different memory management backends.
+ *
+ * http://www.ijg.org/
+ */
+ /**
+ * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
+ */
---
+++ blinky/blinky/asf-3.21.0/thirdparty/ijg/libs/jpg_ijg_without_mm/include/jversion.h
@@ -0,0 +1,17 @@
+/*
+ * jversion.h
+ *
+ * Copyright (C) 1991-1998, Thomas G. Lane.
+ * This file is part of the Independent JPEG Group's software.
+ * For conditions of distribution and use, see the accompanying README file.
+ *
+ * This file contains software version identification.
+ */
+ /**
+ * Support and FAQ: visit <a href="http://www.atmel.com/design-support/">Atmel Support</a>
+ */
+
+
+#define JVERSION	"6b  27-Mar-1998"
+
+#define JCOPYRIGHT	"Copyright (C) 1998, Thomas G. Lane"
